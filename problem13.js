3cdb1479f7b6f5f491581621ac90707dd0502e4c
pijush512 <psagorsarker512@gmail.com>
2026-06-29 20:49:35 +0600

    Solved Problem 13 revers array

---
+++ problem13.js
@@ -0,0 +1,8 @@
+// Write a function to reverse the elements of an array without using the built-in .reverse() method.
+
+
+function reverseArr (arr) {
+  return arr.reverse();
+} 
+
+console.log(reverseArr ([1,2,3,4]));
