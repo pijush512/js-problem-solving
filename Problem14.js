8cbee3003b7cf7291149a624d32e9757ee049409
pijush512 <psagorsarker512@gmail.com>
2026-06-30 22:50:16 +0600

    Solved problem 14 Average of Array

--- Problem14.js
+++ Problem14.js
@@ -1,1 +1,11 @@
-// Problem 14
+// Write a function to find the average of all the numbers in a given array.
+
+function averageOfArry (arr) {
+  let average = 0;
+  for(let i = 0; i < arr.length; i++) {
+    average = average + arr[i];
+  }
+  return average;
+};
+
+console.log(averageOfArry ([1, 2, 3, 4, 5]));
